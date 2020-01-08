721161fabf296b953906078b9d59f9fa6fa0369a
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-08 17:52:57 -0500

    ok

--- app/controllers/hospital_controller.rb
+++ app/controllers/hospital_controller.rb
@@ -6,11 +6,24 @@ class HospitalController < Sinatra::Base
     get '/' do 
         erb :about 
     end 
+    
     get '/hospitals' do 
         @hospitals = Hospital.all
         erb :index
     end
-    
+
+    get '/hospitals/new' do 
+        @hospital=Hospital.new(params)
+        erb :new
+    end 
+    get '/hospitals/:id' do 
+        @hospital = Hospital.find_by_id(params[:id])
+        erb :show 
+    end 
+    post '/hospitals' do 
+        @hospital = Hospital.create(params)
+        redirect to "/hospitals"
+    end 
        
 
 end

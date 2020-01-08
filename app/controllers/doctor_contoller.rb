efe19cf3a5dd49e74d6f1652503ce7db2ea83d78
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-08 16:46:18 -0500

    added edit and delete page

--- app/controllers/doctor_contoller.rb
+++ app/controllers/doctor_contoller.rb
@@ -18,11 +18,29 @@ class DoctorController < Sinatra::Base
         @doctor = Doctor.find_by_id(params[:id])
         erb :show 
     end 
+    get '/doctors/:id/edit' do 
+        @doctor = Doctor.find_by_id(params[:id])
+        erb :edit
+    end 
 
     post '/doctors' do 
         
         @doctor = Doctor.create(params)
         redirect to "/doctors/#{@doctor.id}"
+
+    end 
+
+    patch '/doctors/:id' do 
+        @doctor = Doctor.find(params[:id])
+    
+       @doctor.update(params[:doctor])
+       redirect to "/doctors/#{@doctor.id}"
+    end 
+
+    delete '/doctors/:id' do 
+        @doctor = Doctor.find_by_id(params[:id])
+        @doctor.delete 
+        redirect to "/doctors"
 
     end 
 end
